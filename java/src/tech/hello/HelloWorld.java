4117305ab6574394c9b5fc3b00891ee623a63ad5
Chandu <chandu.cky@gmail.com>
2015-05-01 01:45:36 +0530

    Hello world Program and first commit.

---
+++ java/src/tech/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package tech.hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!!");
+		System.out.println("First Commit");
+	}
+
+}
