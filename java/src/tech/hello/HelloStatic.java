ec517229606e1454bc6315626ba8cc243d482be2
Anshu <anshukas>
2015-05-01 10:40:04 +0530

    To use the normal method in main class, which is not static in nature

---
+++ java/src/tech/hello/HelloStatic.java
@@ -0,0 +1,25 @@
+package tech.hello;
+/**
+ * To use the normal method in main class, which is not static in nature
+ * @author Kansakar
+ *
+ */
+public class HelloStatic {
+	public static void main(String[] args){
+		String msg = sayHello();
+		System.out.println(msg);
+		
+		//Call the method which is not static in nature in main method which is static in nature.
+		String msgAgian= new HelloStatic().sayHelloAgain();
+		System.out.println(msgAgian);
+		
+	}
+	
+	public static String sayHello(){
+		return "Hello I'm in static nature";
+	}
+	
+	public String sayHelloAgain(){
+		return "Hello I'm not in static nature";
+	}
+}
