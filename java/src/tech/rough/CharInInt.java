246e673288789fd8bf04a7a13d842669c3082b94
Anshu <anshubtechint@gmail.com>
2016-03-13 10:51:05 +0530

    update the class with comment.

--- java/src/tech/rough/CharInInt.java
+++ java/src/tech/rough/CharInInt.java
@@ -1,8 +1,12 @@
 package tech.rough;
-
+/**
+ * This ask in some quiz.
+ * @author anshu
+ *
+ */
 public class CharInInt {
 	public static void main(String[] args){
-		char ch = 126;
+		char ch = 326;
 		System.out.println(126+10);
 	}
 }
