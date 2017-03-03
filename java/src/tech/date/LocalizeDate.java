a2682e3ac481d872676ae3fe8e1ae1afd1cfcc94
Anshu Kumar <anshukansakar@gmail.com>
2017-03-03 17:22:43 +0530

    Localize the date

--- java/src/tech/date/LocalizeDate.java
+++ java/src/tech/date/LocalizeDate.java
@@ -1,5 +1,6 @@
 package tech.date;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.Date;
@@ -11,11 +12,15 @@ import java.util.Date;
  */
 
 public class LocalizeDate {
+	private static final DateFormat jsDf = new SimpleDateFormat("EEE MMM dd yyyy HH:mm:ss 'GMT'Z '('z')'");
 	public static void main(String[] args){
 		Locale brLocale = new Locale("pt", "BR");
 		SimpleDateFormat sdf = new SimpleDateFormat("EEEE MMMMM yyyy HH:mm:ss.SSSZ",brLocale);
 		
 		//print current date and time in Brazilian 
 		System.out.println(sdf.format(new Date()));
+		
+		//
+		System.out.println(jsDf.format(new Date()));
 	}
 }
